534aed686d87133cf74b06b86423c85552984901
ace <mr.alihusnain11@gmail.com>
2025-04-27 09:36:49 +0500

    uploaded

---
+++ src/main/java/com/ace/springweb/SpringwebApplication.java
@@ -0,0 +1,14 @@
+package com.ace.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebApplication.class, args);
+		System.out.println("Hello to the Future");
+	}
+
+}
